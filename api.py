8c76d125a38be40d31775de9862bbf401a6f644f
ZuzanaMa <73612519+ZuzanaMa@users.noreply.github.com>
2021-10-17 18:49:38 +0200

    correlation calculation
    example how to calculate correlation between two columns, tuned api.py

--- api.py
+++ api.py
@@ -1,5 +1,10 @@
-
+# pip install requests
 import requests
+
 api_url = "https://data.statistics.sk/api/v2/dataset/om7102rr/all/2020/0/0?lang=sk"
 response = requests.get(api_url)
-print(response.json()) #response.json()['value']
+
+# print value
+print(response.json()['value'])
+# example how to print slovak regions
+print(response.json()['dimension']['om7102rr_vuc']['category']['label'].values()) 
