9942b33af43c87ba0ca873717598c916daec0dff
Zuzana Macicova <zuma132@gmail.com>
2021-11-24 22:33:00 +0100

    DB scheme #20
    db scheme, all tables incnluded

---
+++ dekkProjekt/database/migrations/2021_11_24_202858_create_values_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateValuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('values', function (Blueprint $table) {
+            $table->id();
+            $table->decimal("value");
+            $table->foreignId("district_id")->constrained("districts");
+            $table->foreignId("specific_dataset_id")->constrained("specific_year_datasets");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('values');
+    }
+}
