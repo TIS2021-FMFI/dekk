ff6a3cf1c73deedb5de919bdf8ae76ae5166340a
Marek <sinthoras@hotmail.sk>
2022-01-05 01:11:07 +0100

    Map now correctly displays dataset names
    minor fixes related to git commit

--- dekkProjekt/public/js/load_data.js
+++ dekkProjekt/public/js/load_data.js
@@ -32,18 +32,10 @@ function onResponseValues(){
     if(xmlHttp.readyState == 4 && xmlHttp.status == 200)   {
         response = JSON.parse(xmlHttp.responseText);
 
-<<<<<<< Updated upstream
-        console.log(response);
-
-
-        districts1 = addValueProperty(response, 'dataset1');
-        districts2 = addValueProperty(response, 'dataset2');
-=======
         districts1 = addValueProperty(response, 'dataset1', response['ds1']);
         districts2 = addValueProperty(response, 'dataset2', response['ds2']);
 
         console.log(districts1);
->>>>>>> Stashed changes
 
         MapModule.addLayers(districts1, districts2);
         GraphModule.drawGraph(response);
@@ -77,25 +69,15 @@ function onResponseParameters(){
 }
 
 // modifies geoJSON to contain respective values per district
-<<<<<<< Updated upstream
-function addValueProperty(valuesDict, dataset) {
-    let districts = JSON.parse(JSON.stringify(okresy)); // deep copy okresy into districts, do not modify okresy
-=======
 function addValueProperty(valuesDict, datasetKey, datasetName) {
     let districts = JSON.parse(JSON.stringify(okresy)); // deep copy okresy into districts, do not modify okresy
     districts['datasetName'] = datasetName;
->>>>>>> Stashed changes
 
     for (let i in districts['features']) {
         let district = districts['features'][i]['properties']['NM3'];
 
-<<<<<<< Updated upstream
-        if (valuesDict[dataset].hasOwnProperty(district)) {
-            districts['features'][i]['properties']['value'] = parseFloat(valuesDict[dataset][district]);
-=======
         if (valuesDict[datasetKey].hasOwnProperty(district)) {
             districts['features'][i]['properties']['value'] = parseFloat(valuesDict[datasetKey][district]);
->>>>>>> Stashed changes
         }
     }
 
