191bca90127b8d34a0a978b3cbc5f2068e367f71
Marek <sinthoras@hotmail.sk>
2022-02-04 14:07:19 +0100

    Fixed dropdown select

--- dekkProjekt/resources/views/map_leaflet.blade.php
+++ dekkProjekt/resources/views/map_leaflet.blade.php
@@ -10,14 +10,14 @@
     <!-- leaflet stylesheet -->
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
         integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
-        crossorigin="" />
+        crossorigin="anonymous" />
 
     <!-- other stylesheets -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/app.css" />
+    <link rel="stylesheet" href="css/app.css" crossorigin="anonymous"/>
     <link rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/css/bootstrap-select.css" />
+        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/css/bootstrap-select.css" crossorigin="anonymous"/>
 
     <!-- The code uses small FileSaver.js library to save generated images and Canvas-to-Blob.js library to ensure browser compatibility. Required for graph export. -->
     <script
