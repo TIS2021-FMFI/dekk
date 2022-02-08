b3f463e56841e9d256aa51db257ae841cc001e25
Marek Lichvar <37308362+sinthoras50@users.noreply.github.com>
2022-02-08 15:45:57 +0100

    Update app.js

--- dekkProjekt/resources/js/app.js
+++ dekkProjekt/resources/js/app.js
@@ -3,6 +3,6 @@ require('./bootstrap');
 
 
 // disable console.log for the whole project if not in debugging mode
-const DEBUG_MODE = false;
+const DEBUG_MODE = true;
 
 if (!DEBUG_MODE) console.log = () => {};
